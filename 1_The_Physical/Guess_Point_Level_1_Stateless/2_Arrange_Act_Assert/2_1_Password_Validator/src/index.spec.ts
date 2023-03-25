99d596dba33acc042bd17d07a187b5bc225f3998
vec911git <victor.escudero.cabrera@gmail.com>
2023-03-25 10:25:28 +0100

    [New] Password Validator - Must be between 5 and 15 characters long

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -2,8 +2,15 @@ import { isPasswordValid } from './index'
 
 describe('password validator', () => {
 
-    it('Write a function for validating passwords', () => {
+    it('A function exists for validating passwords', () => {
         expect(typeof(isPasswordValid)).toBe("function");
+    })
+
+    it('Must be between 5 and 15 characters long', () => {
+        expect(isPasswordValid('1')).toBeFalsy();
+        expect(isPasswordValid('12345')).toBeTruthy();
+        expect(isPasswordValid('123451234512345')).toBeTruthy();
+        expect(isPasswordValid('1234512345123451')).toBeFalsy();
     })
 
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.ts
@@ -1,3 +1,5 @@
 export function isPasswordValid(password: string): boolean {
+    if (password.length < 5 || password.length > 15)
+        return false;
     return true;
 }
