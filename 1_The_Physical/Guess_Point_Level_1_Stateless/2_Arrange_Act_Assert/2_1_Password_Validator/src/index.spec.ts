30b163b627287b12e33247fcfe4f0e216bcec7b4
vec911git <victor.escudero.cabrera@gmail.com>
2023-03-25 10:18:12 +0100

    [New] Password Validator - A function exists for validating passwords

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_1_Password_Validator/src/index.spec.ts
@@ -1,5 +1,10 @@
+import { isPasswordValid } from './index'
 
 describe('password validator', () => {
+
+    it('Write a function for validating passwords', () => {
+        expect(typeof(isPasswordValid)).toBe("function");
+    })
 
 })
 
