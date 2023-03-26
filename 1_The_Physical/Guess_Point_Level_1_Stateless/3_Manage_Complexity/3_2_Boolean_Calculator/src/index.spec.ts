f61457d41d8918837240a75e8803d4c383f57377
vec911git <victor.escudero.cabrera@gmail.com>
2023-03-26 20:08:43 +0200

    [New] boolean calculator - Create a boolean calculator

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,8 @@
 
-describe('boolean calculator', () => {
+import { BooleanCalculator } from './index';
 
+describe('boolean calculator', () => {
+    it('Create a boolean calculator', () => {
+        expect(typeof(BooleanCalculator)).toBe("function");
+    })
 })
