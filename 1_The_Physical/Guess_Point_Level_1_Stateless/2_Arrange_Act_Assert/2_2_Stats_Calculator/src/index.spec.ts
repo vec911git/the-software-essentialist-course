3daa99cd240cc48d8c785b34a27f97e04209e618
vec911git <victor.escudero.cabrera@gmail.com>
2023-03-26 11:46:44 +0200

    [New] stats calculator - It calculates minimum value

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -9,6 +9,11 @@ describe('stats calculator', () => {
     it('It process a sequence of integer numbers', () => {
         expect(() => statsCalculator([2, 4, 6])).not.toThrow('All sequence elements must be integers');
         expect(() => statsCalculator([2, 4.5, 6])).toThrow('All sequence elements must be integers');
+        expect(() => statsCalculator([])).toThrow('All sequence elements must be integers');
+    })
+
+    it('It calculates minimum value', () => {
+        expect(statsCalculator([2, 4, 21, -8, 53, 40]).minValue).toEqual(-8);
     })
 
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -1,4 +1,27 @@
-export function statsCalculator(numberSequence: number[]) {
-    if (numberSequence?.some(x => x%1 !== 0))
+export function statsCalculator(numberSequence: number[]): StatHolder {
+    if (numberSequence.length === 0 || numberSequence.some(x => x%1 !== 0))
         throw new Error('All sequence elements must be integers');
+
+    let stats = new StatHolder();
+    let minValue:any = null;
+    for (const element of numberSequence) {
+        if (minValue === null || minValue > element) minValue = element;
+    }
+    stats.minValue = minValue;
+
+    return stats;
+}
+
+class StatHolder {
+    minValue: number;
+    maxValue: number;
+    numberOfElements: number;
+    avgValue: number;
+
+    constructor() {
+        this.minValue = 0;
+        this.maxValue = 0;
+        this.numberOfElements = 0;
+        this.avgValue = 0.0;
+    }
 }
