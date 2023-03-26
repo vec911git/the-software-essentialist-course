e1400d1af1e2acf8de562b9469daf602ecc7982d
vec911git <victor.escudero.cabrera@gmail.com>
2023-03-26 11:16:19 +0200

    [New] stats calculator - It process a sequence of integer numbers

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -6,4 +6,9 @@ describe('stats calculator', () => {
         expect(typeof(statsCalculator)).toBe("function");
     })
 
+    it('It process a sequence of integer numbers', () => {
+        expect(() => statsCalculator([2, 4, 6])).not.toThrow('All sequence elements must be integers');
+        expect(() => statsCalculator([2, 4.5, 6])).toThrow('All sequence elements must be integers');
+    })
+
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -1,3 +1,4 @@
 export function statsCalculator(numberSequence: number[]) {
-    
+    if (numberSequence?.some(x => x%1 !== 0))
+        throw new Error('All sequence elements must be integers');
 }
