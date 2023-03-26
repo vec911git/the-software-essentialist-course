e7f9a48c456380811b1a8e2e9c9faf8501322e67
vec911git <victor.escudero.cabrera@gmail.com>
2023-03-26 10:58:29 +0200

    [New] stats calculator - A function exists for stats calculations

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,9 @@
+import { statsCalculator } from './index';
 
 describe('stats calculator', () => {
 
+    it('A function exists for stats calculations', () => {
+        expect(typeof(statsCalculator)).toBe("function");
+    })
 
 })
