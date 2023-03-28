e41987efee053446c4ce39e47118e29d76d85660
vec911git <victor.escudero.cabrera@gmail.com>
2023-03-28 10:40:06 +0200

    [New] boolean calculator - It takes a boolean expression

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -2,7 +2,17 @@
 import { BooleanCalculator } from './index';
 
 describe('boolean calculator', () => {
+
+    let booleanCalculator = new BooleanCalculator();
+
     it('Create a boolean calculator', () => {
-        expect(typeof(BooleanCalculator)).toBe("function");
+        expect(typeof(booleanCalculator)).toBe("object");
+    })
+
+    it('takes a boolean expression', () => {
+        expect(() => booleanCalculator.addBooleanExpression('')).toThrow('Boolean expression is empty');
+        expect(() => booleanCalculator.addBooleanExpression('RUE')).toThrow('Boolean expression contains invalid values');
+        expect(() => booleanCalculator.addBooleanExpression('MOT TRUE')).toThrow('Boolean expression contains invalid values');
+        expect(() => booleanCalculator.addBooleanExpression('TRUE')).not.toThrow('Boolean expression contains invalid values');
     })
 })
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -1,3 +1,14 @@
 export class BooleanCalculator {
-    
+    private readonly validTokens = ["TRUE", "FALSE", "NOT", "AND", "OR"];
+    private booleanExpression: string = "";
+
+    public addBooleanExpression(booleanExpression: string) {
+        if (booleanExpression.length === 0) 
+            throw new Error('Boolean expression is empty');
+        if (!booleanExpression.split(' ').every(x => this.validTokens.includes(x))) 
+            throw new Error('Boolean expression contains invalid values');
+
+        this.booleanExpression = 
+            this.booleanExpression.concat(booleanExpression);
+    } 
 }
